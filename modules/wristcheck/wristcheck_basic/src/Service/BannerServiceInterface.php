7690f25dac692739da03a17c69481bb6c91b929b
Jinjin123 <1293813551@qq.com>
2020-07-21 07:05:53 +0800

    update home page

--- modules/wristcheck/wristcheck_basic/src/Service/BannerServiceInterface.php
+++ modules/wristcheck/wristcheck_basic/src/Service/BannerServiceInterface.php
@@ -5,7 +5,7 @@ namespace Drupal\wristcheck_basic\Service;
 /**
  * Interface BannerServiceInterface.
  */
-interface BannerServiceInterface
+interface BannerServiceInterface extends BaseServiceInterface
 {
   public function queryBanner(bool $isEnable);
 }
---
+++ modules/wristcheck/wristcheck_basic/src/Service/BaseServiceInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace Drupal\wristcheck_basic\Service;
+
+
+interface BaseServiceInterface
+{
+}
