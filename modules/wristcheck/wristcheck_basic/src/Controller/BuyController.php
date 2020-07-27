a4785a82415c488123e5f90205fda767d77c409f
Jinjin123 <1293813551@qq.com>
2020-07-27 17:42:32 +0800

    clean buy page

--- modules/wristcheck/wristcheck_basic/src/Controller/BuyController.php
+++ modules/wristcheck/wristcheck_basic/src/Controller/BuyController.php
@@ -36,35 +36,8 @@ class BuyController extends ControllerBase implements ContainerInjectionInterfac
 
   public function index()
   {
-    $banners = $this->bannerService->queryBanner(true);
-    $explore_wristcheck_view = views_embed_view('wristcheck_buy_explore');
-    $popular_brands_view = views_embed_view('wristcheck_buy_popular_brands');
-    $news_view = views_embed_view('wristcheck_buy_news');
-    $magazine_view = views_embed_view('wristcheck_buy_magazine');
-//    $testimdnials_view = views_embed_view('wristcheck_testimdnials');
-
-//    $block = \Drupal\block\Entity\Block::load('wristchecksellingprocess');
-//    $wristcheck_selling_process_view = \Drupal::entityTypeManager()->getViewBuilder('block')->view($block);
-//
-//    $download_block = Block::load('wristcheckdownloadappfollowus');
-//    $download_view = \Drupal::entityTypeManager()->getViewBuilder('block')->view($download_block);
-
-
-    $variables = [
-      'banners' => $banners,
-      'magazines' => render($magazine_view),
-      'explore_wristcheck' => render($explore_wristcheck_view),
-      'popular_brands' => render($popular_brands_view),
-//      'testimdnials' => render($testimdnials_view),
-      'news' => render($news_view),
-//      'wristcheck_selling_process' => render($wristcheck_selling_process_view),
-//      'download' =>render($download_view)
-    ];
-
-
     return [
       '#theme' => 'wristcheck_buy',
-      '#variables' => $variables,
 //      '#type' => 'markup',
 //      '#markup' => $this->t('Implement method: index')
     ];
