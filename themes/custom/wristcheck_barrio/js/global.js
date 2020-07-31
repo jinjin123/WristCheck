a69130bc5bdfe2ad5c03abb51bd8c110c6044d98
Jinjin123 <1293813551@qq.com>
2020-07-31 14:31:38 +0800

    change theme fixed buy page style

--- themes/custom/wristcheck_barrio/js/global.js
+++ themes/custom/wristcheck_barrio/js/global.js
@@ -3,18 +3,17 @@
  * Global utilities.
  *
  */
-(function($, Drupal) {
+(function ($, Drupal) {
 
   'use strict';
 
   Drupal.behaviors.bootstrap_barrio_subtheme = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       var position = $(window).scrollTop();
       $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
           $('body').addClass("scrolled");
-        }
-        else {
+        } else {
           $('body').removeClass("scrolled");
         }
         var scroll = $(window).scrollTop();
@@ -34,7 +33,7 @@
   console.log('js loaded success');
 
   //faq dropdown
-  $(".path-wristcheck-faq .views-field-title .field-content i").click(function() {
+  $(".path-wristcheck-faq .views-field-title .field-content i").click(function () {
     target = $(this);
     if (target.parent().parent().parent().parent().parent().children()[2].style.display == "block") {
       target.parent().parent().parent().parent().parent().children()[2].style.display = "none"
@@ -43,27 +42,27 @@
       target.parent().parent().parent().parent().parent().children()[2].style.display = "block"
       target.css("-webkit-transform", "rotate(45deg)");
     }
-  )}
+  })
 // menu show hide
   $('#primary-menu .navbar-nav>li.mega-dropdown').hover(function () {
     if ($(this).find('.mega-dropdown-menu').length > 0) {
-      $('.body-modal').addClass('show');
+      $('.wc-page-modal').addClass('show');
     }
   }, function () {
-    $('.body-modal').removeClass('show');
+    $('.wc-page-modal').removeClass('show');
   });
 //faq system step
-  $('#faq-auth-system').css("background-color","#333")
-  $(".view-wristcheck-contact-us .views-field-title .field-content i").click(function(){
+  $('#faq-auth-system').css("background-color", "#333")
+  $(".view-wristcheck-contact-us .views-field-title .field-content i").click(function () {
     target = $(this);
     // console.log(target.parent().parent().parent().parent().parent().children()[1].style.display="block")
-    if(target.parent().parent().parent().parent().parent().children()[1].style.display=="block"){
-      target.parent().parent().parent().parent().parent().children()[1].style.display="none"
+    if (target.parent().parent().parent().parent().parent().children()[1].style.display == "block") {
+      target.parent().parent().parent().parent().parent().children()[1].style.display = "none"
       // target.parent().parent().parent().parent().parent().children()[1].css("margin-bottom","30px")
-      target.css("-webkit-transform","rotate(-45deg)");
-    }else{
-      target.parent().parent().parent().parent().parent().children()[1].style.display="block"
-      target.css("-webkit-transform","rotate(45deg)");
+      target.css("-webkit-transform", "rotate(-45deg)");
+    } else {
+      target.parent().parent().parent().parent().parent().children()[1].style.display = "block"
+      target.css("-webkit-transform", "rotate(45deg)");
     }
   })
 })(jQuery, Drupal);
