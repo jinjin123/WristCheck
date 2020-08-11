38bdeeffeec56a26619a6680713c0b47d2e07f45
Jinjin123 <hanzijinjin123@gmail.com>
2020-08-11 17:46:20 +0800

    faq

---
+++ modules/wristcheck/wristcheck_basic/src/Controller/FaqAuthSystemStepController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\wristcheck_basic\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class FaqController.
+ */
+class FaqAuthSystemStepController extends ControllerBase {
+
+  /**
+   * Index.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function index() {
+    return [
+      '#theme' => 'wristcheck_faqauthsystem_step',
+      '#type' => 'markup',
+    ];
+  }
+
+}
