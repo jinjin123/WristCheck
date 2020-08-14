f5feb1fbd247eac48aec96ed91953e7e4748a5e7
Jinjin123 <hanzijinjin123@gmail.com>
2020-08-14 11:40:43 +0800

    register user

--- modules/wristcheck/wristcheck_basic/src/Form/UserRegisterForm.php
+++ modules/wristcheck/wristcheck_basic/src/Form/UserRegisterForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\wristcheck_basic\Form;
 
 use Drupal\Core\Link;
+use Drupal\Core\Ajax\RedirectCommand;
 use Drupal\Core\Url;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Form\FormBase;
@@ -14,62 +15,18 @@ use Drupal\Core\Ajax\HtmlCommand;
  */
 class UserRegisterForm extends FormBase
 {
-
-  /**
-   * Drupal\Core\Flood\FloodInterface definition.
-   *
-   * @var \Drupal\Core\Flood\FloodInterface
-   */
-  protected $flood;
-
-  /**
-   * The user storage.
-   *
-   * @var \Drupal\user\UserStorageInterface
-   */
-  protected $userStorage;
-
-  /**
-   * Drupal\user\UserAuthInterface definition.
-   *
-   * @var \Drupal\user\UserAuthInterface
-   */
-  protected $userAuth;
-
-  /**
-   * Drupal\Core\Render\RendererInterface definition.
-   *
-   * @var \Drupal\Core\Render\RendererInterface
-   */
-  protected $renderer;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container)
-  {
-    $instance = parent::create($container);
-    return $instance;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function getFormId()
   {
-    return 'user_login_form';
+    return 'user_register_form';
   }
-
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state)
   {
-    // get user login form.
-//    $form = \Drupal::formBuilder()->getForm('Drupal\user\Form\UserLoginForm');
      $form['Email'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Email'),
+      '#title' => 'Email',
       '#size' => 60,
       '#maxlength' => USERNAME_MAX_LENGTH,
       '#description' => $this->t('Enter your  Email.') ,
@@ -79,13 +36,13 @@ class UserRegisterForm extends FormBase
         'autocapitalize' => 'none',
         'spellcheck' => 'false',
         'autofocus' => 'autofocus',
-        'placeholder' => $this->t('Email'),
+        'placeholder' => 'Email',
       ],
     ];
 
     $form['Name'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Username'),
+      '#title' => 'Username',
       '#size' => 60,
       '#maxlength' => USERNAME_MAX_LENGTH,
       '#description' => $this->t('Enter your username.' ),
@@ -95,16 +52,19 @@ class UserRegisterForm extends FormBase
         'autocapitalize' => 'none',
         'spellcheck' => 'false',
         'autofocus' => 'autofocus',
-        'placeholder' => $this->t('Username'),
+        'placeholder' => 'Username',
       ],
     ];
-
+    $form['message'] = [
+      '#type' => 'markup',
+      '#markup' => '<div class="result_message"></div>'
+    ];
     $form['actions'] = [
       '#type' => 'button',
       '#value' => $this->t('register'),
       '#ajax' => array(
 //        'callback' => array($this, 'validateAjax'),
-//        'callback' => '::submitForm',
+//        'callback' => 'Drupal\wristcheck_basic\Form\UserRegisterForm::submitForm',
         'event' => 'click',
         'url' => Url::fromRoute('wristcheck_basic.user_register'),
         'progress' => array(
@@ -113,21 +73,8 @@ class UserRegisterForm extends FormBase
         ),
       ),
     ];
+//    @TODO: return notice mssage for user when login faild
     return $form;
-//    $config = $this->config('system.site');
-//    $form['message'] = [
-//      '#type' => 'markup',
-//      '#markup' => '<div class="result_message"></div>'
-//    ];
-//
-//    $form['links'] = [
-//      '#type' => 'markup',
-//      '#markup' => '<div class="links"><a class="register" href="' . $base_url . '/user/register">' . $this->t('No user account yet?') . '</a><a class="forget" href="' . $base_url . '/user/password">' . $this->t('Forget Password?') . '</a></div>',
-//      '#suffix'=>'<div class="form-footer text-center"><p class="form-footer-title">Do you not currently have a user account?</p><div><a class="wc-btn-dark"><div class="wc-btn-cont"><span class="fa fa-arrow-right"></span> | <span>IN THE CONTINUE</span></div></a></div></div>'
-//    ];
-//
-//    $this->renderer->addCacheableDependency($form, $config);
-//    return $form;
   }
 
 
@@ -149,28 +96,38 @@ class UserRegisterForm extends FormBase
    */
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
-//    $account = $this->userStorage->load($form_state->get('uid'));
-//    // A destination was set, probably on an exception controller,
-//    if (!$this->getRequest()->request->has('destination')) {
-//      $form_state->setRedirect(
-//        'entity.user.canonical',
-//        ['user' => $account->id()]
+
+//    $response = new AjaxResponse();
+//    $url = '/my_module/my_controller?selected_value=' . $selectedValue;
+//    $response->addCommand(new RedirectCommand($url));
+//    return $response;
+//    dpm("The AJAX FUNCTION has ran...");
+//    \Drupal::logger('User_Register')->error('user register faild
+//' . json_encode($form_state));
+//    $response = new AjaxResponse();
+//    \Drupal::logger('User_Register')->error('user register faild' . json_encode($form_state));
+//    if ($form_state->hasAnyErrors()) {
+//      $error = $this->t('User Exits, Please change')->render();
+//      $response = new AjaxResponse();
+//      $response->addCommand(
+//        new HtmlCommand(
+//          '.result_message',
+//          '<div class="result_message">' . 'fdsafsafsaf' . '</div>')
 //      );
-//    } else {
-//      $this->getRequest()->query->set('destination', $this->getRequest()->request->get('destination'));
 //    }
-//
-//    user_login_finalize($account);
-    $response = new AjaxResponse();
-    $response->addCommand(
-      new HtmlCommand(
-        'wc-header',
-        '<div class="head wc-header">The result is ' . t('The results is ') . ($form_state->getValue('Email') + $form_state->getValue('Name')) . '</div>')
-    );
-    return $response;
+//    else {
+//      $url = Url::fromRoute('wristcheck_basic.user_controller_useractivate)');
+//      $command = new RedirectCommand($url->toString());
+//      $response->addCommand($command);
+//    }
+//    return $response;
   }
-
-
+//
+//  public function UserRegisterCallback(array &$form, FormStateInterface $form_state){
+//    $response = new AjaxResponse();
+//    $response->addCommand(new HtmlCommand('.result_message', 'user not exits'));
+//    return $response;
+//  }
   /**
    * Checks supplied username/password against local users table.
    *
@@ -219,53 +176,5 @@ class UserRegisterForm extends FormBase
 //    }
   }
 
-  /**
-   * Checks if user was not authenticated, or if too many logins were attempted.
-   *
-   * This validation function should always be the last one.
-   */
-  public function validateFinal(array &$form, FormStateInterface $form_state)
-  {
-//    $flood_config = $this->config('user.flood');
-//    $error = array();
-//    if (!$form_state->get('uid')) {
-//      // Always register an IP-based failed login event.
-//      $this->flood->register('user.failed_login_ip', $flood_config->get('ip_window'));
-//      // Register a per-user failed login event.
-//      if ($flood_control_user_identifier = $form_state->get('flood_control_user_identifier')) {
-//        $this->flood->register('user.failed_login_user', $flood_config->get('user_window'), $flood_control_user_identifier);
-//      }
-//
-//      if ($flood_control_triggered = $form_state->get('flood_control_triggered')) {
-//        if ($flood_control_triggered == 'user') {
-//          $error[] = $this->t('There has been more than one failed login attempt for this account. It is temporarily blocked. Try again later or <a href=":url">request a new password</a>.', 'There have been more than @count failed login attempts for this account. It is temporarily blocked. Try again later or <a href=":url">request a new password</a>.', [':url' => $this->url('user.pass')]);
-//        } else {
-//          // We did not find a uid, so the limit is IP-based.
-//          $error[] = $this->t('Too many failed login attempts from your IP address. This IP address is temporarily blocked. Try again later or <a href=":url">request a new password</a>.', [':url' => $this->url('user.pass')]);
-//        }
-//      } else {
-//        // Use $form_state->getUserInput() in the error message to guarantee
-//        // that we send exactly what the user typed in. The value from
-//        // $form_state->getValue() may have been modified by validation
-//        // handlers that ran earlier than this one.
-//        $user_input = $form_state->getUserInput();
-//        $query = isset($user_input['name']) ? ['name' => $user_input['name']] : [];
-//        $error[] = $this->t('Unrecognized username or password. <a href=":password">Forgot your password?</a>', [':password' => $this->url('user.pass', [], ['query' => $query])]);
-//        $accounts = $this->userStorage->loadByProperties(['name' => $form_state->getValue('name')]);
-//        if (!empty($accounts)) {
-//          $error[] = $this->t('Login attempt failed for %user.', ['%user' => $form_state->getValue('name')]);
-//        } else {
-//          // If the username entered is not a valid user,
-//          // only store the IP address.
-//          $error[] = $this->t('Login attempt failed from %ip.', ['%ip' => $this->getRequest()->getClientIp()]);
-//        }
-//      }
-//    } elseif ($flood_control_user_identifier = $form_state->get('flood_control_user_identifier')) {
-//      // Clear past failures for this user so as not to block a user who might
-//      // log in and out more than once in an hour.
-//      $this->flood->clear('user.failed_login_user', $flood_control_user_identifier);
-//    }
-//    return $error;
-  }
 
 }
