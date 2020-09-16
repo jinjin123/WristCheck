6d21fe0bd2275803545a8e3e25b5c9c91f319576
csi0n <123123123>
2020-09-16 17:44:17 +0800

    update

--- modules/wristcheck/wristcheck_basic/src/Plugin/Block/WristcheckFilterProductBlock.php
+++ modules/wristcheck/wristcheck_basic/src/Plugin/Block/WristcheckFilterProductBlock.php
@@ -20,6 +20,21 @@ class WristcheckFilterProductBlock extends BlockBase {
   public function build() {
     $build = [];
     $build['#theme'] = 'wristcheck_filter_product_block';
+
+    $entityManager = \Drupal::service('entity_field.manager');
+    $fields = $entityManager->getFieldDefinitions('commerce_product', 'watch');
+
+    $fieldLocationDefinition = $fields['field_location']->getFieldStorageDefinition();
+    $locations = $fieldLocationDefinition->getSettings()['allowed_values'];
+
+    $currencies = $this->commerceCurrencyRepository->getAll();
+
+    $variables = [
+      'locations' => $locations,
+      'currencies' => $currencies
+    ];
+
+    $build['#variables'] = $variables;
 //     $build['wristcheck_filter_product_block']['#markup'] = 'Implement WristcheckFilterProductBlock.';
 
     return $build;
