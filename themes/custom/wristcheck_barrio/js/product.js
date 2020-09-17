7a20425113b18349ebf254054de7b3e39f9eea92
Jinjin123 <1293813551@qq.com>
2020-09-17 18:10:41 +0800

    product and menu

--- themes/custom/wristcheck_barrio/js/product.js
+++ themes/custom/wristcheck_barrio/js/product.js
@@ -20,30 +20,26 @@
     });
     // wishlist end
 
-    // product img slick
-    $('.wc-product-imglist1 .view-content').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      nextArrow: '',
-      prevArrow: '',
-      centerPadding: '0',
-      asNavFor: '.wc-product-imglist2 .view-content'
+    $(".etalage").zoom({
+      align: "left",              // 当前展示图片的位置，则放大的图片在其相对的位置
+      thumb_image_width: 680,     // 当前展示图片的宽
+      thumb_image_height: 500,    // 当前展示图片的高
+      source_image_width: 2040,    // 放大图片的宽
+      source_image_height: 1500,  // 放大图片的高
+      zoom_area_width: 700,       // 放大图片的展示区域的宽
+      zoom_area_height: 700,// 放大图片的展示区域的高
+      zoom_area_distance: 10,     //
+      zoom_easing: true,          // 是否淡入淡出
+      description_opacity: 0.7,
+      small_thumbs: 3,            // 小图片展示的数量
+      smallthumb_inactive_opacity: 0.4,   // 小图片处于非激活状态时的遮罩透明度
+      smallthumbs_position: "bottom",     // 小图片的位置
+      show_icon: true,
+      hide_cursor: false,         // 鼠标放到图片时，是否隐藏指针
+      speed: 600,                 //
+      autoplay: true,             // 是否自动播放
+      autoplay_interval: 6000,    // 自动播放时每张图片的停留时间
     });
-    $('.wc-product-imglist2 .view-content').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      asNavFor: '.wc-product-imglist1 .view-content',
-      dots: false,
-      arrows: false,
-      centerPadding: '0',
-      prevArrow: '',
-      nextArrow: '',
-      centerMode: true,
-      focusOnSelect: true
-    });
-    // product img slick end
 
   });
 })(jQuery);
