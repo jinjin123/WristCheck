2f54eea39028bc73b53b633ffa3d7d7fac5d91a4
xjs <carlxjs@qq.com>
2020-11-18 14:33:46 +0800

    add product in cart, calucate the insurance again

--- modules/wristcheck/wristcheck_basic/src/EventSubscriber/CartEventSubscriber2.php
+++ modules/wristcheck/wristcheck_basic/src/EventSubscriber/CartEventSubscriber2.php
@@ -2,7 +2,9 @@
 namespace Drupal\wristcheck_basic\EventSubscriber;
 
 use Drupal\commerce_cart\CartManagerInterface;
+use Drupal\commerce_cart\Event\CartEntityAddEvent;
 use Drupal\commerce_cart\Event\CartEvents;
+use Drupal\commerce_order\Adjustment;
 use Drupal\commerce_order\Entity\Order;
 use Drupal\Core\Messenger\MessengerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -43,7 +45,8 @@ class CartEventSubscriber2 implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     return [
-      CartEvents::CART_EMPTY => [['emptyCart', 100]]
+      CartEvents::CART_EMPTY => [['emptyCart', 100]],
+      CartEvents::CART_ENTITY_ADD => [['entityAddCart', 100]],
     ];
   }
 
@@ -55,4 +58,31 @@ class CartEventSubscriber2 implements EventSubscriberInterface {
      $order->save();
   }
 
+  public function entityAddCart(CartEntityAddEvent $event) {
+    $cart =  $event->getCart();
+    $order_id = $cart->id();
+    $order = Order::load($order_id);
+    $subTotalPrice = $order->getSubtotalPrice();
+    $insurance_rate = \Drupal::config('wristcheck_basic.rateSettings')->get('insurance');
+    $adjustment_amount = $subTotalPrice->multiply($insurance_rate)->divide('100');
+    $adjustment = new Adjustment([
+      'type' => 'custom',
+      'label' => 'Insurance Plus '. $insurance_rate .'%',
+      'amount' => $adjustment_amount,
+      'percentage' => bcdiv($insurance_rate, '100', 3),
+      'included' => FALSE,
+      'locked' => TRUE,
+    ]);
+    $field_if_need_insurance = $order->get('field_if_need_insurance')->getValue();
+    if (isset($field_if_need_insurance[0]['value']) && $field_if_need_insurance[0]['value']) {
+      $order->set('field_if_need_insurance', 1);
+      $order->setAdjustments([$adjustment]);
+      $order->save();
+    }
+    else {
+      $order->set('field_if_need_insurance', 0);
+      $order->setAdjustments([]);
+      $order->save();
+    }
+  }
 }
